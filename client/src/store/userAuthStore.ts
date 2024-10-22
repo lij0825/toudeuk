b3199a1cbbb8097353fd68d89485b931f3017b80
이인준 <dldlswns890@naver.com>
2024-10-22 14:52:49 +0900

    [FEAT] base response 타입 / 클라이언트 api

---
+++ client/src/store/userAuthStore.ts
@@ -0,0 +1,22 @@
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+
+interface AuthState {
+  accessToken: string | null;
+  setAccessToken: (accessToken: string) => void;
+}
+
+export const useAuthStore = create(
+  persist<AuthState>(
+    (set) => ({
+      accessToken: "",
+      setAccessToken: (accessToken) => {
+        set({ accessToken });
+      },
+    }),
+    {
+      name: "auth-store",
+      storage: createJSONStorage(() => sessionStorage),
+    }
+  )
+);
