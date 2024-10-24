1ec2293406a684092d1ebcce92fb07dde8881e14
이인준 <dldlswns890@naver.com>
2024-10-24 09:44:39 +0900

    [FEAT] 인증 권한 예외 처리

--- server/src/main/java/com/toudeuk/server/core/exception/ErrorCode.java
+++ server/src/main/java/com/toudeuk/server/core/exception/ErrorCode.java
@@ -11,6 +11,7 @@ public enum ErrorCode {
 	// Common
 	SERVER_ERROR(1000, HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러가 발생하였습니다."),
 	INVALID_INPUT_VALUE(1001, HttpStatus.BAD_REQUEST, "유효하지 않은 입력 값입니다."),
+	ACCESS_DENIED(1002, HttpStatus.FORBIDDEN, "접근 권한이 없습니다."),
 
 	// User
 	USER_NOT_FOUND(2000, HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
@@ -21,14 +22,14 @@ public enum ErrorCode {
 	USER_ITEM_NOT_FOUND(3001, HttpStatus.NOT_FOUND, "사용자의 아이템을 찾을 수 없습니다."),
 	USER_ITEM_ALREADY_USED(3002, HttpStatus.BAD_REQUEST, "이미 사용한 아이템입니다."),
 
-    USER_NOT_EXISTS(1002, HttpStatus.NOT_FOUND, "해당 회원은 존재하지 않습니다."),
+	USER_NOT_EXISTS(1002, HttpStatus.NOT_FOUND, "해당 회원은 존재하지 않습니다."),
 
-    NOT_VALID_BEARER_GRANT_TYPE(401, HttpStatus.UNAUTHORIZED, "인증 타입이 Bearer 타입이 아닙니다."),
-    NOT_EXISTS_AUTHORIZATION(401, HttpStatus.UNAUTHORIZED, "Authorization Header가 빈값입니다."),
-    NOT_ACCESS_TOKEN_TYPE(1003, HttpStatus.UNAUTHORIZED, "해당 토큰은 access token이 아닙니다."),
-    FORBIDDEN_ADMIN(404, HttpStatus.FORBIDDEN, "관리자 Role이 아닙니다.");
+	NOT_VALID_BEARER_GRANT_TYPE(401, HttpStatus.UNAUTHORIZED, "인증 타입이 Bearer 타입이 아닙니다."),
+	NOT_EXISTS_AUTHORIZATION(401, HttpStatus.UNAUTHORIZED, "Authorization Header가 빈값입니다."),
+	NOT_ACCESS_TOKEN_TYPE(1003, HttpStatus.UNAUTHORIZED, "해당 토큰은 access token이 아닙니다."),
+	FORBIDDEN_ADMIN(404, HttpStatus.FORBIDDEN, "관리자 Role이 아닙니다.");
 
-    private final int code;
-    private final HttpStatus status;
-    private final String message;
+	private final int code;
+	private final HttpStatus status;
+	private final String message;
 }
