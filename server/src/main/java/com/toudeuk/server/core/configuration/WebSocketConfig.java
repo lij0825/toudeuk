64f793e41801aa53b38455894a15ab2fabd27e6d
SeongMin Hwang <qq221qq@naver.com>
2024-10-24 15:11:59 +0900

    [FEAT] redis template

--- server/src/main/java/com/toudeuk/server/core/configuration/WebSocketConfig.java
+++ server/src/main/java/com/toudeuk/server/core/configuration/WebSocketConfig.java
@@ -7,13 +7,13 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
-@EnableWebSocketMessageBroker //웹 소켓 메시지를 다룰 수 있게 허용
+@EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
-		config.enableSimpleBroker("/topic"); //발행자가 "/topic"의 경로로 메시지를 주면 구독자들에게 전달
-		config.setApplicationDestinationPrefixes("/app"); // 발행자가 "/app"의 경로로 메시지를 주면 가공을 해서 구독자들에게 전달
+		config.enableSimpleBroker("/topic");
+		config.setApplicationDestinationPrefixes("/app");
 	}
 
 	@Override
