1862a07f8afe85f4e2ca1971e5886c1914e1b2b9
sgo722@naver.com <sgo722@naver.com>
2024-10-28 09:26:50 +0900

    [fix] OAuth2 재설정

--- server/src/main/java/com/toudeuk/server/domain/user/repository/UserRepository.java
+++ server/src/main/java/com/toudeuk/server/domain/user/repository/UserRepository.java
@@ -1,9 +1,9 @@
 package com.toudeuk.server.domain.user.repository;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.toudeuk.server.domain.user.entity.User;
@@ -11,4 +11,8 @@ import com.toudeuk.server.domain.user.entity.User;
 public interface UserRepository extends JpaRepository<User, Long> {
 
 	Optional<User> findByName(String name);
+
+	@Query("SELECT u FROM User u WHERE u.username = :username")
+	Optional<User> findByUsername(@Param("username") String username);
+
 }
