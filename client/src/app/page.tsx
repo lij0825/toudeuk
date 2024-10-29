9c2c29782e71b46e18cd9154bb681ec34366aa02
박보람 <142669102+gittidev@users.noreply.github.com>
2024-10-29 13:37:07 +0900

    [FEAT] 로그인 구조 변경

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,23 +1,26 @@
-"use client";
-
-import { Suspense } from "react";
-import dynamic from "next/dynamic";
-
+// import { Suspense } from "react";
+// import dynamic from "next/dynamic";
+import Login from "./componnents/Login";
 // HomeIcon을 동적으로 import하여 에러 격리
-const HomeIcon = dynamic(() => import("./componnents/HomeIcon"), {
-  loading: () => <div>Loading icon...</div>,
-  ssr: false,
-});
+// const HomeIcon = dynamic(() => import("./componnents/HomeIcon"), {
+//   loading: () => <div>Loading icon...</div>,
+//   ssr: false,
+// });
 
 export default function Home() {
   return (
-    <div className="p-td flex flex-col justify-center w-full h-full min-h-screen">
-      <Suspense fallback={<div>Loading...</div>}>
-        <HomeIcon />
-      </Suspense>
+    <div className="p-td flex flex-col justify-center w-full h-full min-h-screen relative">
+      {/* HomeIcon을 배경에 위치 */}
+      <div className="absolute top-0 left-0 w-full h-full z-0">
+        {/* <Suspense fallback={<div>Loading...</div>}>
+          <HomeIcon />
+        </Suspense> */}
+      </div>
 
-      <h1 className="typo-title">터득</h1>
-      <h1 className="typo-title">TouDeuk</h1>
+      {/* Login을 그 위에 배치 */}
+      <div className="relative z-10">
+        <Login />
+      </div>
     </div>
   );
 }
