a65bf979e947d03b295fe4c503216b9d21dbe9b2
SeongMin Hwang <qq221qq@naver.com>
2024-10-31 16:15:46 +0900

    [FEAT] 백엔드 소켓 수정

--- server/src/main/java/com/toudeuk/server/tmp/GameClickController.java
+++ server/src/main/java/com/toudeuk/server/tmp/GameClickController.java
@@ -3,6 +3,7 @@ package com.toudeuk.server.tmp;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 
@@ -17,17 +18,42 @@ public class GameClickController {
 
 	private static Integer cnt = 0;
 	private final JWTService jwtService;
+	private final SimpMessagingTemplate messagingTemplate;
+
 
 	@MessageMapping("/game")
-	@SendTo("/topic/game")
-	public Integer greeting(@Header("Authorization") String bearerToken) throws Exception {
+	public void sendPublish(@Header("Authorization") String bearerToken) throws Exception {
 		;
 		Long userId = resolveToken(bearerToken);
-
 		System.out.println(userId + " 유저 아이디 ");
+		// 특정 사용자에게만 메시지 전송
+		messagingTemplate.convertAndSendToUser(
+			userId.toString(),
+			"/queue/game",
+			cnt
+		);
 
-		return ++cnt;
+		messagingTemplate.convertAndSend("/topic/game", cnt);
 	}
+
+
+	@MessageMapping("/game")
+	public void sendUser(@Header("Authorization") String bearerToken) throws Exception {
+		;
+		Long userId = resolveToken(bearerToken);
+		
+		messagingTemplate.convertAndSendToUser(
+			userId.toString(),
+			"/queue/game/" + userId,
+			cnt
+		);
+
+		//userId번 에게 전송
+		messagingTemplate.convertAndSend("/topic/game", cnt);
+	}
+
+
+
 
 	private Long resolveToken(String bearerToken) {
 		if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
