b878cb5bd8be59b883ed061278d6af5753099413
박보람 <142669102+gittidev@users.noreply.github.com>
2024-11-01 17:11:04 +0900

    [FEAT] 프로필 수정
    프로필 수정은 되는데
    이미지가 캐시반영이 바로 안되는데
    수정하겠습니다.

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -38,6 +38,11 @@ const nextConfig = {
         hostname: "mblogthumb-phinf.pstatic.net",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "d2uaylxt6f2ffw.cloudfront.net",
+        pathname: "/**",
+      },
     ],
   },
   webpack(config) {
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,23 +1,10 @@
-// import { Suspense } from "react";
-// import dynamic from "next/dynamic";
+import { Suspense } from "react";
+import dynamic from "next/dynamic";
 import Login from "./componnents/Login";
-// HomeIcon을 동적으로 import하여 에러 격리
-// const HomeIcon = dynamic(() => import("./componnents/HomeIcon"), {
-//   loading: () => <div>Loading icon...</div>,
-//   ssr: false,
-// });
 
 export default function Home() {
   return (
     <div className="p-td flex flex-col justify-center w-full h-full min-h-screen relative">
-      {/* HomeIcon을 배경에 위치 */}
-      <div className="absolute top-0 left-0 w-full h-full z-0">
-        {/* <Suspense fallback={<div>Loading...</div>}>
-          <HomeIcon />
-        </Suspense> */}
-      </div>
-
-      {/* Login을 그 위에 배치 */}
       <div className="relative z-10">
         <Login />
       </div>
