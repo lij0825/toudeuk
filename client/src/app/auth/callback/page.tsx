062f0f1a174e7b5d197fdf4671c476095b9f3d01
박보람 <142669102+gittidev@users.noreply.github.com>
2024-11-04 19:00:26 +0900

    [FIX] navbar 경로 수정

--- client/src/app/auth/callback/page.tsx
+++ client/src/app/auth/callback/page.tsx
@@ -1,23 +1,22 @@
-"use client"
-import { useWebSocketStore } from '@/store/useWebSocketStore ';
-import React, { useEffect } from 'react'
+"use client";
+import { useWebSocketStore } from "@/store/useWebSocketStore ";
+import React, { useEffect } from "react";
 
-export default function useOauth () {
+export default function useOauth() {
   const connect = useWebSocketStore((state) => state.connect);
   useEffect(() => {
-    if (typeof window !== 'undefined') { // 클라이언트 측에서만 실행
+    if (typeof window !== "undefined") {
+      // 클라이언트 측에서만 실행
       const params = new URLSearchParams(window.location.search);
-      const token = params.get('accessToken');
+      const token = params.get("accessToken");
       console.log(token);
 
       if (token) {
-        sessionStorage.setItem('accessToken', token);
+        sessionStorage.setItem("accessToken", token);
         connect(token);
-        window.location.href = '/toudeuk';
+        window.location.href = "/toudeuk";
       }
     }
   }, [connect]);
-  return (
-    <div>Loading</div>
-  )
+  return <div className="">Loading</div>;
 }
--- client/src/app/toudeuk/page.tsx
+++ client/src/app/toudeuk/page.tsx
@@ -1,11 +1,11 @@
 "use client";
-import CurrentRank from "./components/CurrentRank";
+
 import GameButton from "./components/GameButton";
 
 export default function Toudeuk() {
   return (
-    <div className="flex flex-col items-center h-full">
-      <CurrentRank rank={7} />
+    <div className="flex flex-col items-center justify-center h-full">
+      {/* <CurrentRank rank={7} /> */}
       <GameButton />
     </div>
   );
