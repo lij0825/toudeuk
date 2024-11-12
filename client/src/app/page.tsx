68df3b17861c50b4c5be492d518ff612c441a845
박보람 <142669102+gittidev@users.noreply.github.com>
2024-11-12 21:57:18 +0900

    [FIX] 로그인 수정

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import dynamic from "next/dynamic";
+import { useRef, useState } from "react";
 import Login from "./components/Login";
 import { CUSTOM_ICON } from "@/constants/customIcons";
 
@@ -10,8 +11,18 @@ const LottieAnimation = dynamic(
 );
 
 export default function Home() {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const [clickCount, setClickCount] = useState(0);
+
+  const handleClick = (e: React.MouseEvent) => {
+    setClickCount((prevCount) => prevCount + 1);
+  };
+
   return (
-    <div className="relative flex flex-col items-center justify-center w-full h-full bg-white overflow-hidden">
+    <div
+      onClick={handleClick}
+      className="relative flex flex-col items-center justify-center w-full h-full bg-white overflow-hidden"
+    >
       <div className="absolute w-full h-full flex flex-col items-center justify-center">
         <div className="font-noto text-3xl font-extrabold">터득</div>
         <div className="typo-sub-title">Toudeuk</div>
@@ -27,6 +38,7 @@ export default function Home() {
             <Login />
           </div>
         </div>
+        <div className="text-gray-400 font-noto">터득이란?</div>
       </div>
     </div>
   );
