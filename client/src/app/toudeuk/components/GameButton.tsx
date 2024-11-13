20a0087562a3e2ff8f0f467bc5981a3eb8d47d28
yjkim9497 <yj009977@gmail.com>
2024-11-13 16:49:18 +0900

    [FIX] 메인페이지 css 수정

--- client/src/app/toudeuk/components/GameButton.tsx
+++ client/src/app/toudeuk/components/GameButton.tsx
@@ -1,48 +1,41 @@
 "use client";
 
-//소켓 연결 또는 SSE 방식으로 touch값 fetch
+import LottieAnimation from "@/app/components/LottieAnimation";
+import { CUSTOM_ICON } from "@/constants/customIcons";
+// 소켓 연결 또는 SSE 방식으로 touch값 fetch
 import { useState } from "react";
+
 interface GameProps {
   totalClick: number;
 }
 
 export default function GameButton({ totalClick }: GameProps) {
-
   return (
     <>
       {/* 테두리가 회전하는 버튼 */}
-      <div
+      <button
         data-cy="button"
-        className="absolute w-40 h-40 rounded-full border-2 border-[#00ff88] hover:border-[#ff00ff] transition-colors duration-300 animate-spin-border"
-      ></div>
-      {/* 수신된 count가 있을 때만 표시 */}
-      {totalClick !== null && (
-        <span
-          className="z-10 typo-title text-3xl text-[#00ff88] hover:text-[#ff00ff] transition-colors duration-300 "
-          style={{ pointerEvents: "none" }}
-        >
-          {totalClick}
-        </span>
-      )}
-      <style jsx>{`
-        @keyframes spinBorder {
-          0% {
-            transform: rotate(0deg);
-            border-color: #00ff88;
-          }
-          50% {
-            border-color: #ff00ff;
-          }
-          100% {
-            transform: rotate(360deg);
-            border-color: #00ff88;
-          }
-        }
+        className="flex flex-col items-center relative" // relative 추가
+      >
+        {/* Lottie 애니메이션 */}
+        <LottieAnimation
+          animationData={CUSTOM_ICON.snowframe}
+          loop={true}
+          autoplay={true}
+          width={250}
+          height={250}
+        />
 
-        .animate-spin-border {
-          animation: spinBorder 2s linear infinite;
-        }
-      `}</style>
+        {/* 수신된 count가 있을 때만 표시 */}
+        {totalClick !== null && (
+          <span
+            className="absolute top-1/2 transform -translate-y-1/2 z-10 text-4xl text-[#00ff88] hover:text-[#ff00ff] transition-colors duration-300"
+            style={{ pointerEvents: "none" }}
+          >
+            {totalClick}
+          </span>
+        )}
+      </button>
     </>
   );
 }
