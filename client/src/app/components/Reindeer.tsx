2ad88dd53b587bdccae36c64a7cb8b7a3ec65343
박보람 <142669102+gittidev@users.noreply.github.com>
2024-11-14 18:01:08 +0900

    [FIX] 루돌프

--- client/src/app/components/Reindeer.tsx
+++ client/src/app/components/Reindeer.tsx
@@ -20,25 +20,31 @@ export default function Reindeer() {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  console.log(containerWidth);
-
   return (
     <div
       ref={containerRef}
-      style={{ overflow: "hidden", position: "relative" }}
+      style={{
+        overflow: "hidden",
+        position: "relative",
+        width: "100%", // 부모 컨테이너 크기 설정
+        height: 120, // Lottie 애니메이션 크기보다 약간 크게 설정
+      }}
     >
       {containerWidth > 0 && (
         <motion.div
-          initial={{ x: containerWidth, rotateY: 180 }} // 오른쪽 끝에서 시작
+          initial={{ x: -100, rotateY: 180 }} // 애니메이션 시작 위치 조정
           animate={{
-            x: [containerWidth, -containerWidth, containerWidth],
-            rotateY: [180, 0, 180], // 양 끝에서만 방향 전환
+            x: [containerWidth + 100, -100], // LottieAnimation이 좌우 바깥으로 이동
+            rotateY: [0, 180, 0], // 양 끝에서만 방향 전환
           }}
           transition={{
-            duration: 10,
+            duration: containerWidth / 20,
             ease: "easeInOut",
             repeat: Infinity,
             times: [0, 0.5, 1], // 양 끝에서만 rotateY 전환
+          }}
+          style={{
+            position: "absolute", // 내부 애니메이션 요소가 부모 내에서 절대 위치 이동
           }}
         >
           <LottieAnimation
